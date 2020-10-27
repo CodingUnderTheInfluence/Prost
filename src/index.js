ba4edb37ec4dec77bc9e74726a5d987c701d9e43
Christopher Booth <christopherbooth@ChristophersMBP.attlocal.net>
2020-10-27 13:48:13 -0500

    (add) make Create component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Create from './components/Create.jsx';
 const Index = () => {
   return (
     <>
       <div>Welcome to React!</div>
       <div>Hello world!</div>
+      <Create />
     </>
   );
 };
