3be8dc01510d7299c15c3293a30fcf99baeb2fce
lschwall <opsparklschwall@gmail.com>
2020-10-28 16:52:06 -0500

    (add) login and logout owrking

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,12 +3,15 @@ import { GoogleLogin } from 'react-google-login';
 import { Button } from '@material-ui/core';
 import Axios from 'axios';
 
-const clientId = `838925295609-rh25afhf4it1rstq51pumhspvr7r09j7.apps.googleusercontent.com`
+const clientId = ``
 
 function Login() {
     const onSuccess = (res) => {
         console.log('[LOGIN SUCCESS] currentUser:', res.profileObj);
         Axios.get('/db/customers')
+        // .then(({ data }) => {
+        //     console.log(data, 'DATA')
+        // })
     }
 
     const onFailure = () => {
