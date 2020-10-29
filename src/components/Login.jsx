ff3ffc44c8845c36807a9d2dd186fc1aed02dbe6
Christopher Booth <christopherbooth@ChristophersMBP.attlocal.net>
2020-10-29 15:19:06 -0500

    (setup) update app jsx landing page with login / logout

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { GoogleLogin } from 'react-google-login';
 
-const clientId ='933644302187-agamsig0qalm5oi4fd44v11hfffpchs8.apps.googleusercontent.com'
+const clientId = '933644302187-agamsig0qalm5oi4fd44v11hfffpchs8.apps.googleusercontent.com'
 
 const Login = () => {
     const onSuccess = (res) => {
         console.log('[Login Success] currentUser:', res.profileObj)
+        const token = res.tokenId;
+        const params = {
+            authToken: token
+        }
+        localStorage.setItem('token', res.tokenId)
+        console.log(localStorage);
+        Axios.post('/db/customer', { params })
     }
 
     const onFailure = (res) => {
@@ -19,7 +26,7 @@ const Login = () => {
                 onSuccess={onSuccess}
                 onFailure={onFailure}
                 cookiePolicy={'single_host_origin'}
-                style={{marginTop: '100px'}}
+                style={{ marginTop: '100px' }}
                 isSignedIn={true}
             />
         </div>
