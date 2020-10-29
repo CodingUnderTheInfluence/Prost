080cc45972c9bf057330ec3c01802612862473c5
Christopher Booth <christopherbooth@ChristophersMBP.attlocal.net>
2020-10-29 15:26:56 -0500

    (add) conditional rendering based on token

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import { render } from 'react-dom';
 import LandingPage from './components/LandingPage.jsx';
+import CustomerView from './components/Customer/CustomerView.jsx';
+import OwnerView from './components/Owner/OwnerView.jsx';
 
 function App() {
+
   const [value, setValue] = useState('');
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -14,11 +17,17 @@ function App() {
     }
   })
 
-  return(
-    <div>
-      <LandingPage />
-    </div>
-  )
+    if (value === 'Landing') {
+      return <LandingPage />
+    } else if (value === 'CustomerView') {
+      return <CustomerView />
+    } else if (value === 'OwnerView') {
+      return <OwnerView />
+    } else {
+      return (
+        <div> Loading...</div>
+      )
+    }
   // const renderView = () => {
   //     if (value === 'Landing') {
   //         return <LandingPage />
