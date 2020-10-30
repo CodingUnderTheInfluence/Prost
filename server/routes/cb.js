33f2029fc97c6efb32ce454fc023ab99b857443f
Bri-b <bri.skinner02@gmail.com>
2020-10-29 19:12:34 -0500

    (connect) grab all customer bars

--- server/routes/cb.js
+++ server/routes/cb.js
@@ -26,6 +26,21 @@ cbRouter.get('/', (req, res) => {
   });
 })
 
+cbRouter.get('/history/:customerId', (req, res) => {
+  const {customerId} = req.params;
+  Customers_Bars.findAll({
+    where: {
+      id_customer: customerId
+    }
+  })
+  .then((cbs) => {
+    res.send(cbs);
+  })
+  .catch((err) => {
+    res.status(500).send(err);
+  });
+})
+
   // 
 module.exports = {
   cbRouter,
--- src/components/Customer/Profile/History.jsx
+++ src/components/Customer/Profile/History.jsx
@@ -3,7 +3,27 @@ import { Grid, Typography, Button } from '@material-ui/core'
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 
-export default function History({setView}) {
+export default function History({setView, customerId}) {
+  const [data, setData] = useState(null);
+
+  const getData = () => {
+    fetch(`${process.env.REDIRECT}/db/cb/history/${customerId}`, {
+      method: 'GET',
+    })
+    .then(response => response.json())
+    .then(res => {
+      const [result] = res;
+      console.log('Contact Success :', result)
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return(
     <div>
       <ArrowBackIosIcon color="primary" onClick={()=> setView('Home')} />
