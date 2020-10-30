d50a1a6cd8d5d1501326384830e7e2044cb0a36c
Bri-b <bri.skinner02@gmail.com>
2020-10-30 14:49:26 -0500

    (connect) favorite view

--- src/components/Customer/Profile/Favorite.jsx
+++ src/components/Customer/Profile/Favorite.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect, useLayoutEffect} from 'react';
 import { Grid, Typography, Button } from '@material-ui/core'
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import DeleteOutlinedIcon from  '@material-ui/icons/DeleteOutlined'
 import HistoryList from './HistoryList.jsx'
 
 export default function Favorite({setView, customerId}) {
@@ -18,6 +19,24 @@ export default function Favorite({setView, customerId}) {
       console.error('Error:', error);
     });
   }
+  const deleteFavorite = async (num) => {
+    try {
+      const obj = {
+        id_bar: num,
+        id_customer: customerId
+      }
+      const result = await fetch(`${process.env.REDIRECT}/db/cb/delete/favorite`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+      })
+      getData();
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   useEffect(() => {
     getData();
@@ -32,6 +51,7 @@ export default function Favorite({setView, customerId}) {
         <p>{bar.bar_name}</p>
         <p>{bar.address}</p> 
         <p>{bar.phone_number}</p> 
+        <DeleteOutlinedIcon onClick={() => deleteFavorite(bar.id)}/>
       </div>
       ))} 
     </div>
--- src/components/Customer/Profile/HistoryList.jsx
+++ src/components/Customer/Profile/HistoryList.jsx
@@ -1,15 +1,36 @@
 import React, {useState, useEffect, useLayoutEffect} from 'react';
 import { Grid, Typography, Button } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add';
 
 
-export default function HistoryList({ list }) {
-  const [data, setData] = useState(null);
+export default function HistoryList({ list, customerId }) {
+  const addFavorite = async () => {
+    try {
+      const obj = {
+        id_bar: list.id,
+        id_customer: customerId
+      }
+      const result = await fetch(`${process.env.REDIRECT}/db/cb/add/favorite`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(obj),
+      })
+      console.log(result);
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   return(
     <div>
       <p>{list.bar_name}</p>
       <p>{list.address}</p> 
       <p>{list.phone_number}</p> 
+      <div>
+        <AddIcon onClick={addFavorite}/>
+      </div>
     </div>
   )
 }
