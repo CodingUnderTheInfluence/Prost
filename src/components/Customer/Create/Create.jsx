a395ab44d8c0513aa693cfc667cfb982b4712716
tenhold <tenhold@gmail.com>
2020-10-30 18:14:09 -0500

    (Move) create to own folder

---
+++ src/components/Customer/Create/Create.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Create = () => {
+    return (
+        <div>Hi from Create!</div>
+    )
+}
+
+const QuickCreate = () => {
+
+
+    function success(pos) {
+      var crd = pos.coords;
+    
+      console.log('Your current position is:');
+      console.log(`Latitude : ${crd.latitude}`);
+      console.log(`Longitude: ${crd.longitude}`);
+      console.log(`More or less ${crd.accuracy} meters.`);
+    }
+    
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    
+    const handleClick = () => {
+      navigator.geolocation.getCurrentPosition(success, error);
+    };
+    
+    
+      return (
+        <button onClick={handleClick}>I'm drinking!</button>
+      );
+    };
+    
+export default Create;
---
+++ src/components/Customer/Create/CustomerView.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { makeStyles, Paper, Tabs, Tab, Grid, Button, Typography } from '@material-ui/core';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import ForumIcon from '@material-ui/icons/Forum';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import Create from './Create.jsx';
+import FriendsList from '../Social/FriendsList.jsx';
+import Messages from '../Social/Messages.jsx';
+import Profile from '../Profile/CustomerProfile.jsx';
+
+
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 500,
+    },
+});
+
+const CustomerView = () => {
+    const classes = useStyles();
+    const [value, setValue] = useState();
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const renderView = () => {
+        if (value === 0) {
+            return <FriendsList />
+        }
+        if (value === 1) {
+            return <Create />
+        }
+        if (value === 2) {
+            return <Messages />
+        }
+        if (value === 3) {
+          return <Profile />
+      }
+        return (<div>PLACE INSTRUCTIONS HERE</div>)
+    }
+    return (
+        <Grid container direction="column" justify="center" alignItems="center">
+            <Grid item container direction="row" justify="center" alignItems="center">
+                {renderView()}
+            </Grid>
+            <Grid item container direction="row" justify="center" alignItems="center">
+                <Paper className={classes.root}>
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        centered
+                    >
+                        <Tab icon={<PeopleAltIcon />} label="Friends" />
+                        <Tab icon={<AddCircleOutlineIcon />} label="Create" />
+                        <Tab icon={<ForumIcon />} label="Messages" />
+                        <Tab icon={<AccountCircleOutlinedIcon />} label="Profile" />
+                    </Tabs>
+                </Paper>
+            </Grid>
+        </Grid>
+    );
+}
+export default CustomerView;
