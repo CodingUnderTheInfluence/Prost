c83fbf33fa18f110db2659f014180d36f9e2bf87
Bri-b <bri.skinner02@gmail.com>
2020-10-30 12:36:47 -0500

    (connect) favorites list

--- src/components/Customer/Profile/Favorite.jsx
+++ src/components/Customer/Profile/Favorite.jsx
@@ -1,12 +1,39 @@
 import React, {useState, useEffect, useLayoutEffect} from 'react';
 import { Grid, Typography, Button } from '@material-ui/core'
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import HistoryList from './HistoryList.jsx'
 
-export default function Favorite({setView}) {
+export default function Favorite({setView, customerId}) {
+  const [list, setList] = useState(null);
+  const getData = () => {
+    fetch(`${process.env.REDIRECT}/db/cb/favorite/${customerId}`, {
+      method: 'GET',
+    })
+    .then(response => response.json())
+    .then(res => {
+      console.log('Contact Success :', res)
+      setList(res);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return(
     <div>
       <ArrowBackIosIcon color="primary" onClick={()=> setView('Home')} />
-      Hello from Favorite 
+      Hello from Favorite
+      {list && (list.map((bar, key) => 
+        <div key={key}>
+        <p>{bar.bar_name}</p>
+        <p>{bar.address}</p> 
+        <p>{bar.phone_number}</p> 
+      </div>
+      ))} 
     </div>
   )
 }
