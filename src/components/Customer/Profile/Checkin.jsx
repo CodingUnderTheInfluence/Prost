0c8f4a24b6a099c427e94a1ab3529de099bc4fe6
Bri-b <bri.skinner02@gmail.com>
2020-11-02 16:51:01 -0600

    (feat) draft 1 of check in

--- src/components/Customer/Profile/Checkin.jsx
+++ src/components/Customer/Profile/Checkin.jsx
@@ -1,13 +1,107 @@
-import React, {useState, useEffect, useLayoutEffect} from 'react';
-import { Grid, Typography, Button } from '@material-ui/core'
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import axios from 'axios';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import Search from '../Create/Search.jsx'
+import mapStyle from '../../../helpers/mapStyle';
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
 
+const libraries = ['places'];
+
+const mapStyles = {
+  width: '98vw',
+  height: '75vh'
+};
+
+const options = {
+  zoomControl: true,
+  scaleControl: false,
+  mapTypeControl: false,
+  fullscreenControl: false,
+  styles: mapStyle
+};
+
+const searchBox = {
+  boxSizing: `border-box`,
+  border: `1px solid transparent`,
+  width: `240px`,
+  height: `32px`,
+  padding: `0 12px`,
+  borderRadius: `3px`,
+  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+  fontSize: `14px`,
+  outline: `none`,
+  textOverflow: `ellipses`,
+  position: "absolute",
+  left: "50%",
+  marginLeft: "-120px"
+};
 
 export default function Checkin({setView}) {
+  const [text, setText] = useState('');
+  const [currentPosition, setCurrentPosition] = useState({
+    lat: 29.951065,
+    lng: -90.071533
+  });
+  const [publicLocations, setPublicLocations] = useState([]);
+  const [friendLocations, setFriendLocations] = useState([]);
+  const [selectedItem, setSelectedItem] = useState({});
+  const [myLocation, setMyLocation] = useState({});
+  const [markers, setMarkers] = useState([]);
+  const [parties, setParties] = useState([]);
+  const [searchMarker, setSearchMarker] = useState({});
+  const [click, setClick] = useState(false);
+  const [placeInfo, setplaceInfo] = useState(null);
+
+  const defaultCenter = {
+    lat: 29.951065,
+    lng: -90.071533,
+  };
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    libraries
+  });
+
+  useEffect(() => {
+    let isMounted = true;
+    if (isMounted) {
+      axios.get('/db/bar/all')
+        .then(({ data }) => {
+          setParties(data);
+        });
+    }
+    return () => { isMounted = false };
+  }, []);
+
+  // sets the makers to the user click
+  // save reference to map to use it later and not reload state
+  const mapRef = useRef();
+
+  // move map to the where the user has searched
+
+  // get places info from search bar
+  const getPlaceInfo = useCallback((results) => {
+    const { lat, lng } = results;
+    setSearchMarker({ lat, lng });
+    setplaceInfo(results);
+  }, []);
+
+  if (loadError) {
+    return 'Error loading maps';
+  }
+  if (!isLoaded) {
+    return 'Loading maps';
+  }
   return(
     <div>
       <ArrowBackIosIcon color="primary" onClick={()=> setView('Home')} />
       Hello from Checkin 
+      <Search
+        // panTo={panTo}
+        currentPosition={currentPosition}
+        searchBox={searchBox}
+        getPlaceInfo={getPlaceInfo}
+      />
       </div>
     )
 }
