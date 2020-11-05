18f7d44979c57c696a5e15dcf45859e97f75863f
tenhold <tenhold@gmail.com>
2020-11-05 01:21:49 -0600

    (Fix) issue with friendslist not rendering

--- src/components/Customer/Create/FriendsMarkers.jsx
+++ src/components/Customer/Create/FriendsMarkers.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Marker } from '@react-google-maps/api';
 
-const FriendsMarkers = (({ friendLocations }) => {
-
+const FriendsMarkers = ({ friendLocations }) => {
   return (
-    friendLocations !== undefined && friendLocations.filter(({ isPrivate }) => !isPrivate)
+    friendLocations ? friendLocations.filter(({ isPrivate }) => !isPrivate)
       .map(({ latitude, longitude, gId }) => (
         <Marker
           key={gId}
           position={{
             lat: +latitude,
-            lng: +longitude
+            lng: +longitude,
           }}
         // icon={{url: '../../../../images/iconmonstr-beer-5.svg'}}
         />
       ))
+      : null
   );
-});
+};
 
 export default FriendsMarkers;
