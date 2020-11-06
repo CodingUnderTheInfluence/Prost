258c0178f1f18fb90a51f95ae9d6694a089f0f83
tenhold <tenhold@gmail.com>
2020-11-06 13:33:15 -0600

    (Add) directions and google maps api call, bug keeps calling on each rerender

--- src/components/Customer/Directions/Directions.jsx
+++ src/components/Customer/Directions/Directions.jsx
@@ -10,33 +10,18 @@ const Directions = () => {
 
   const directionsCallback = (response) => {
 
-    if (response) {
-      console.log('directions inside if!!!!!', response)
+    if (response !== null) {
       response.status === 'OK' ? setResponse(() => response)
         : console.log('direction response: ', response);
     }
   };
 
-  const getOrigin = (ref) => {
-    setOrigin(ref);
-  };
-
-  const getDestination = (ref) => {
-    setDestination(ref);
-  };
-
-  const onClick = () => {
-    // if (origin.value)
-  };
-
   return (
     <>
-      <input />
-      <input />
       <DirectionsService
         options={{
-          origin: '1217 Magazine St, New Orleans, LA 70130, USA',
-          destination: '500 Chartres St, New Orleans, LA 70130, USA',
+          origin: origin,
+          destination: destination,
           travelMode: 'DRIVING'
         }}
         callback={directionsCallback}
@@ -49,49 +34,5 @@ const Directions = () => {
   );
 };
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////                                            ///////////////////////////////////////////
-////////////////////////////////////              Real api call                 ///////////////////////////////////////////
-///////////////////////////////////                                            ///////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// const Directions = () => {
-//   const [response, setResponse] = useState(null);
-//   const [origin, setOrigin] = useState('');
-//   const [destination, setDestination] = useState('');
-
-//   const directionsCallback = ((res) => {
-//     if (!res) {
-//       res.status === 'OK' ? setResponse(() => res)
-//         : console.log('direction response: ', res);
-//     }
-//   });
-
-//   const getOrigin = (ref) => {
-//     setOrigin(ref);
-//   };
-
-//   const getDestination = (ref) => {
-//     setDestination(ref);
-//   };
-
-//   const onClick = () => {
-//     // if (origin.value)
-//   };
-
-//   return (
-//     <>
-//       <DirectionsService
-//         options={{
-//           origin: origin,
-//           destination: destination,
-//           travelMode: 'DRIVING'
-//         }}
-//         callback={directionsCallback}
-//       />
-//       {response && <DirectionsRenderer options={{ directions: response }} onLoad={(dirs) => console.log('directions: ', dirs)} />}
-//     </>
-//   );
-// };
 
 export default Directions;
