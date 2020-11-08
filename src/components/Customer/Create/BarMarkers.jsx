05fb6f76e963c35b8f32dfa7f4b6a7b138c199f5
tenhold <tenhold@gmail.com>
2020-11-07 23:56:17 -0600

    (Add) friends markers on screen and names on click

--- src/components/Customer/Create/BarMarkers.jsx
+++ src/components/Customer/Create/BarMarkers.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
-// import { Marker, InfoWindow } from '@react-google-maps/api';
+import React from 'react';
 import BarMarker from './BarMarker.jsx';
-
 
 const BarMarkers = ({ parties }) => {
   return (
@@ -10,50 +8,5 @@ const BarMarkers = ({ parties }) => {
     ))
   );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const BarMarkers = ({ parties }) => {
-//   const [show, setShow] = useState('');
-
-//   const handleClick = (id) => {
-//     // setInfo({ lat: e.latLng.lat(), lng: e.latLng.lng() });
-//     setShow(id);
-//   };
-//   return (
-//     <div>
-//       {parties.length && parties.map(({ bar_name, latitude, longitude, id }) => {
-//         return (
-
-//           <Marker
-//             key={id}
-//             position={{
-//               lat: +latitude,
-//               lng: +longitude,
-//             }}
-//             onClick={() => handleClick(id)}
-//           >
-//             {show && <InfoWindow>
-//               <div>{bar_name}</div>
-//             </InfoWindow>}
-//           </Marker>
-//         )
-//       })}
-//     </div >
-//   );
-// }
 
 export default BarMarkers;
