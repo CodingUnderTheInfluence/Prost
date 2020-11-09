20666836e52f8980595c81d17bf6e48ad4952f43
lschwall <opsparklschwall@gmail.com>
2020-11-09 00:47:06 -0600

    (update) owners can now see customers that have checked into their bar

--- src/components/Owner/BarCustomerList.jsx
+++ src/components/Owner/BarCustomerList.jsx
@@ -7,15 +7,28 @@ const BarList = ({ customerList, barId }) => {
         return (
             <Grid container direction="column" justify="center" alignItems="center">
                 <Grid item container direction="row" justify="center" alignItems="center">
+<<<<<<< HEAD
                     <Typography variant="subtitle1">
                         No Customers Currently Checked In
                     </Typography>
+=======
+                    <h1>No Customers Checked In</h1>
+>>>>>>> ab46038... (update) owners can now see customers that have checked into their bar
                 </Grid>
             </Grid>
         )
     } else {
         return (
+<<<<<<< HEAD
             <CustomerList customerList={customerList} barId={barId} />
+=======
+            <Grid container direction="column" justify="center" alignItems="center">
+                <Grid item container direction="row" justify="center" alignItems="center">
+                    <h1>CUSTOMER LIST</h1>
+                </Grid>
+                {customerList.map(customer => { return <CustomerEntry customer={customer} /> })}
+            </Grid>
+>>>>>>> ab46038... (update) owners can now see customers that have checked into their bar
         )
     }
 }
--- src/components/Owner/CustomerEntry.jsx
+++ src/components/Owner/CustomerEntry.jsx
@@ -5,6 +5,7 @@ import PhoneIcon from '@material-ui/icons/Phone';
 import axios from 'axios';
 
 
+<<<<<<< HEAD
 const CustomerEntry = ({ customer, barId }) => {
 
     const [customerData, setCustomerData] = useState();
@@ -12,6 +13,16 @@ const CustomerEntry = ({ customer, barId }) => {
     const getCustomerData = () => {
         axios.get(`/db/customer/getFriendById?customerId=${customer}`)
             .then(({ data }) => {
+=======
+const CustomerEntry = ({ customer }) => {
+
+    const [customerData, setCustomerData] = useState();
+    const getCustomerData = () => {
+        console.log(typeof customer, 'CUSTOMER')
+        axios.get(`/db/customer/getFriendById?customerId=${customer}`)
+            .then(({ data }) => {
+                console.log(data, 'DATA')
+>>>>>>> ab46038... (update) owners can now see customers that have checked into their bar
                 setCustomerData(data)
             })
             .catch(err => console.warn(err))
@@ -21,6 +32,7 @@ const CustomerEntry = ({ customer, barId }) => {
         getCustomerData();
     }, [])
 
+<<<<<<< HEAD
     if (customerData) {
         return (
             <Grid container direction="column" justify="center" alignItems="center" style={{ border: "solid black 1px", borderRadius: "5px", marginTop: "5px", marginBottom: "5px" }}>
@@ -39,6 +51,22 @@ const CustomerEntry = ({ customer, barId }) => {
                     <Grid item container direction="column" xs={4} justify="center" alignItems="center">
                         Current total: {drink}
                     </Grid>
+=======
+
+
+    if (customerData) {
+        return (
+            <Grid container direction="column" justify="center" alignItems="center" style={{ border: "solid black 1px", borderRadius: "5px" }}>
+                <Grid item container direction="row" justify="center" alignItems="center">
+                    <Typography variant="subtitle1">
+                        {`${customerData.first_name} ${customerData.last_name}`}
+                    </Typography>
+                </Grid>
+                <Grid item container direction="row" justify="center" alignItems="center">
+                    <Typography variant="subtitle2">
+                        {customerData.phone_number} <a href={`tel:+1${customerData.phone_number}`} ><PhoneIcon /></a>
+                    </Typography>
+>>>>>>> ab46038... (update) owners can now see customers that have checked into their bar
                 </Grid>
             </Grid>
         )
