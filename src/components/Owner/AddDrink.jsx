d8c4ee550b206220e1e0755a59309d19858a475c
lschwall <opsparklschwall@gmail.com>
2020-11-11 17:07:45 -0600

    (update) customers list for owner now dynamically renders the total drinks

--- src/components/Owner/AddDrink.jsx
+++ src/components/Owner/AddDrink.jsx
@@ -1,25 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PlusOneIcon from '@material-ui/icons/PlusOne';
 import axios from 'axios';
 
 
-const AddDrink = ({ id, barId }) => {
+const AddDrink = ({ id, barId, setDrinkCount }) => {
 
-    const [count, setCount] = useState(1);
-    const [drink, setDrinkCount] = useState(0);
+    const [count, setCount] = useState(0);;
 
     const addDrink = () => {
+        //
         axios.get(`/db/drinks/drinksList?id=${id}`)
             .then(({ data }) => {
                 if (data === 0) {
                     axios.post('/db/drinks/drinksList', { id, barId })
                 } else if (data === 'Drinks') {
                     axios.put('/db/drinks/updateCount', { count, id, barId })
-                        .then(res => console.log(res))
+                        .then(() => setCount(count++))
                         .catch(err => console.log(err))
                 }
             })
+            .catch((err) => console.warn(err))
     }
+
+    const drinkTotal = () => {
+        axios.get(`/db/drinks/count?customerId=${id}&barId=${barId}`)
+            .then(({ data }) => {
+                setDrinkCount(data);
+            })
+    }
+
+    useEffect(() => {
+        drinkTotal();
+    }, [])
+
 
     const updateCount = () => {
         setCount(count + 1);
@@ -33,6 +46,7 @@ const AddDrink = ({ id, barId }) => {
                 onClick={() => {
                     updateCount();
                     addDrink();
+                    drinkTotal();
                 }} />
         </div>
     )
