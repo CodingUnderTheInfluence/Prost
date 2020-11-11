004da2a0c28e2ef81eb7486836df5f5d292d6753
lschwall <opsparklschwall@gmail.com>
2020-11-11 00:30:42 -0600

    (update) occupency now dynamically updates as users are added

--- src/components/Owner/BarCustomerList.jsx
+++ src/components/Owner/BarCustomerList.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, Typography, Divider } from '@material-ui/core';
 import axios from 'axios';
-import CustomerEntry from './CustomerEntry.jsx'
+import CustomerEntry from './CustomerEntry.jsx';
+import PhoneIcon from '@material-ui/icons/Phone';
+import PlusOneIcon from '@material-ui/icons/PlusOne';
 
 const BarList = ({ barId, setCount }) => {
     const [customerList, setCustomerList] = useState([]);
@@ -35,6 +37,23 @@ const BarList = ({ barId, setCount }) => {
             <Grid container direction="column" justify="center" alignItems="center">
                 <Grid item container direction="row" justify="center" alignItems="center">
                     <h1>CUSTOMER LIST</h1>
+                </Grid>
+                <Grid item container direction="row" justify="center" alignItems="center" style={{ border: "solid 5px black", height: "5px" }}>
+                </Grid>
+                <Grid item container direction="row" justify="center" alignItems="center">
+                    <Grid item container direction="column" xs={3} justify="center" alignItems="center">
+                        <Typography variant="subtitle1">
+                            Name
+                        </Typography>
+                    </Grid>
+                    <Grid item container direction="column" justify="center" alignItems="center" xs={3}>
+                        Call
+                    </Grid>
+                    <Grid item container direction="column" xs={3} justify="center" alignItems="center">
+                        Add Drink
+                    </Grid>
+                </Grid>
+                <Grid item container direction="row" justify="center" alignItems="center" style={{ border: "solid 5px black", height: "5px" }}>
                 </Grid>
                 {customerList.map(customer => { return <CustomerEntry customer={customer} /> })}
             </Grid>
