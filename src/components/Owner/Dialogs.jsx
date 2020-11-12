b9c33a2de34cbbd55a5fa5f51234f6bff3679b6d
lschwall <opsparklschwall@gmail.com>
2020-11-12 12:31:03 -0600

    (update) dialogs now work successfully after breaking into smaller components

---
+++ src/components/Owner/Dialogs.jsx
@@ -0,0 +1,122 @@
+import React, { useState, useEffect } from 'react';
+import {
+    Grid,
+    Typography,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Paper
+} from '@material-ui/core';
+
+const Dialogs = ({
+    occupencyStatus,
+    barAddress,
+    barNumber,
+    barName,
+    capacity,
+    handleClickOpen,
+    handleClickOpenInfo,
+    handleClickOpenOcc,
+    handleCloseInfo,
+    handleCloseOcc,
+    handleClose,
+    openInfo,
+    openOcc,
+    open,
+    count
+}) => {
+
+    return (
+        <div>
+            {/* THIS IS THE INFO DIALOG */}
+            <Dialog
+                open={openInfo}
+                onClose={handleCloseInfo}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Bar Information"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        <Grid container direction="column" justify="center" alignItems="center">
+                            <Paper>
+                                <Grid item container direction="row" justify="center" alignItems="center">
+                                    <Typography variant="subtitle1">
+                                        Bar: {barName}
+                                    </Typography>
+                                </Grid>
+                                <Grid item container direction="row" justify="center" alignItems="center">
+                                    <Typography variant="subtitle2">
+                                        Address: {barAddress}
+                                    </Typography>
+                                </Grid>
+                                <Grid item container direction="row" justify="center" alignItems="center">
+                                    <Typography variant="subtitle2">
+                                        PhoneNumber: {barNumber}
+                                    </Typography>
+                                </Grid>
+                                <Grid item container direction="row" justify="center" alignItems="center">
+                                    <Typography variant="subtitle2">
+                                        Bar Capacity: {capacity}
+                                    </Typography>
+                                </Grid>
+                            </Paper>
+                        </Grid>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseInfo} color="primary" autoFocus>
+                        Close
+                        </Button>
+                </DialogActions>
+            </Dialog>
+
+            {/* THIS IS THE OCCUPENCY DIALOG */}
+            <Dialog
+                open={openOcc}
+                onClose={handleCloseOcc}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Occupency Information"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        {count}
+                        {occupencyStatus()}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseOcc} color="primary" autoFocus>
+                        Close
+                        </Button>
+                </DialogActions>
+            </Dialog>
+
+
+            {/* THIS IS A NORMAL DIALOG */}
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"This will have information"}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        This Dialog will include information regarding this button's title
+                        </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary" autoFocus>
+                        Close
+                        </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
+}
+
+export default Dialogs
--- src/components/Owner/OwnerProfile.jsx
+++ src/components/Owner/OwnerProfile.jsx
@@ -10,8 +10,8 @@ import {
     DialogTitle,
     Paper
 } from '@material-ui/core';
-import axios from 'axios';
 import Logout from '../Logout.jsx';
+import Dialogs from './Dialogs.jsx'
 
 const OwnerProfile = ({
     setViewValue,
@@ -22,38 +22,36 @@ const OwnerProfile = ({
     barNumber,
     capacity
 }) => {
+
     /*
-    This opens info Dialog
+        This opens info Dialog
     */
     const [openInfo, setOpenInfo] = useState(false);
     const handleClickOpenInfo = () => {
         setOpenInfo(true);
     };
-
     const handleCloseInfo = () => {
         setOpenInfo(false);
     };
     /*
-    This opens Occupency Dialog
+        This opens Occupency Dialog
     */
     const [openOcc, setOpenOcc] = useState(false);
     const handleClickOpenOcc = () => {
         setOpenOcc(true);
     };
-
     const handleCloseOcc = () => {
         setOpenOcc(false);
     };
     /*
-    This opens normal Dialog
+        This opens normal Dialog
     */
     const [open, setOpen] = useState(false);
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
     const handleClose = () => {
         setOpen(false);
+    };
+    const handleClickOpen = () => {
+        setOpen(true);
     };
 
 
@@ -102,94 +100,24 @@ const OwnerProfile = ({
                 Call emergency Contact
                 </Button>
             <Logout setViewValue={setViewValue} />
-
-
-            {/* THIS IS THE INFO DIALOG */}
-            <Dialog
-                open={openInfo}
-                onClose={handleCloseInfo}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">{"Bar Information"}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        <Grid container direction="column" justify="center" alignItems="center">
-                            <Paper>
-                                <Grid item container direction="row" justify="center" alignItems="center">
-                                    <Typography variant="subtitle1">
-                                        Bar: {barName}
-                                    </Typography>
-                                </Grid>
-                                <Grid item container direction="row" justify="center" alignItems="center">
-                                    <Typography variant="subtitle2">
-                                        Address: {barAddress}
-                                    </Typography>
-                                </Grid>
-                                <Grid item container direction="row" justify="center" alignItems="center">
-                                    <Typography variant="subtitle2">
-                                        PhoneNumber: {barNumber}
-                                    </Typography>
-                                </Grid>
-                                <Grid item container direction="row" justify="center" alignItems="center">
-                                    <Typography variant="subtitle2">
-                                        Bar Capacity: {capacity}
-                                    </Typography>
-                                </Grid>
-                            </Paper>
-                        </Grid>
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCloseInfo} color="primary" autoFocus>
-                        Close
-                        </Button>
-                </DialogActions>
-            </Dialog>
-
-            {/* THIS IS THE OCCUPENCY DIALOG */}
-            <Dialog
-                open={openOcc}
-                onClose={handleCloseOcc}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">{"Occupency Information"}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        {count}
-                        {occupencyStatus()}
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleCloseOcc} color="primary" autoFocus>
-                        Close
-                        </Button>
-                </DialogActions>
-            </Dialog>
-
-
-            {/* THIS IS A NORMAL DIALOG */}
-            <Dialog
+            <Dialogs
+                occupencyStatus={occupencyStatus}
+                barAddress={barAddress}
+                barNumber={barNumber}
+                barName={barName}
+                capacity={capacity}
+                handleClickOpen={handleClickOpen}
+                handleClickOpenInfo={handleClickOpenInfo}
+                handleClickOpenOcc={handleClickOpenOcc}
+                handleCloseInfo={handleCloseInfo}
+                handleCloseOcc={handleCloseOcc}
+                handleClose={handleClose}
+                openInfo={openInfo}
+                openOcc={openOcc}
                 open={open}
-                onClose={handleClose}
-                aria-labelledby="alert-dialog-title"
-                aria-describedby="alert-dialog-description"
-            >
-                <DialogTitle id="alert-dialog-title">{"This will have information"}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        This Dialog will include information regarding this button's title
-                        </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary" autoFocus>
-                        Close
-                        </Button>
-                </DialogActions>
-            </Dialog>
+                count={count}
+            />
         </Grid>
-
     )
 }
 
