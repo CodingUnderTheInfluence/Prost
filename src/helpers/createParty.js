18d12c3dc390e41ffe7cc7f1d4e01bf8f2c85b6c
tenhold <tenhold@gmail.com>
2020-11-12 17:18:17 -0600

    (Refactor) createParty to how it was before

--- src/helpers/createParty.js
+++ src/helpers/createParty.js
@@ -2,73 +2,41 @@ const axios = require('axios');
 
 /**
  * 
- * @param {Object} barInfo : 
- * @param {Number} size :
+ * @param {Object} barInfo : info about the bar
+ * @param {String} customerId : google id of the current logged in customer
+ * @param {Number} size : of party
  * takes in an object that is retreved from places api and a party size
  * if no party size then size is 1
  * adds info into the database
  */
 
-const createParty = async (barInfo, customerId, size = 1) => {
+const createParty = async (barInfo, size = 1) => {
   const {
     name,
     formatted_address,
     formatted_phone_number,
     geometry: { location },
   } = barInfo;
-
-  const [address, city, stateZip] = formatted_address.split(', ');
-  const [state, zip] = stateZip.split(' ');
   const lat = location.lat();
   const lng = location.lng();
 
+  const bar = await axios.post('/db/bar/create/party', {
+    bar_name: name,
+    phone_number: formatted_phone_number,
+    address: formatted_address,
+    latitude: lat,
+    longitude: lng,
+  });
+  const barId = bar.data[0].id;
   try {
-    const custParty = await axios.post('/db/cb/checkin/create', {
-      barName: name,
-      address: address,
-      city: city,
-      state: state,
-      zip: zip,
-      number: formatted_phone_number,
-      customerId,
-      lat: lat,
-      lng: lng,
+    await axios.post('db/party/create', {
+      id_bar: barId,
+      size,
     });
-    console.info('party created!', custParty);
+    console.info('success in creating party')
   } catch (err) {
-    console.warn('error in party create', err);
+    console.warn('error in create party');
   }
-
 };
-
-
-// const createParty = async (barInfo, size = 1) => {
-//   const {
-//     name,
-//     formatted_address,
-//     formatted_phone_number,
-//     geometry: { location },
-//   } = barInfo;
-//   const lat = location.lat();
-//   const lng = location.lng();
-
-//   const bar = await axios.post('/db/bar/create/party', {
-//     bar_name: name,
-//     phone_number: formatted_phone_number,
-//     address: formatted_address,
-//     latitude: lat,
-//     longitude: lng,
-//   });
-//   const barId = bar.data[0].id;
-//   try {
-//     await axios.post('db/party/create', {
-//       id_bar: barId,
-//       size,
-//     });
-//     console.info('success in creating party')
-//   } catch (err) {
-//     console.warn('error in create party');
-//   }
-// };
 
 module.exports = createParty;
