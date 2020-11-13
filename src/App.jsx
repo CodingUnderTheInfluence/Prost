ebb830f308aec0b176cf74ace59bbdac03042e33
lschwall <opsparklschwall@gmail.com>
2020-11-13 16:11:57 -0600

    (update) rebase 4:12 CST

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,79 @@
 import React, { useState, useEffect, Context } from 'react';
 import {
-    Route,
-    Switch,
-    BrowserRouter
-} from "react-router-dom";
-import { useHistory } from 'react-router-dom'
+  Route,
+  Switch,
+  BrowserRouter,
+  useHistory,
+} from 'react-router-dom';
 import LandingPage from './components/LandingPage.jsx';
 import CustomerView from './components/Customer/CustomerView.jsx';
 import OwnerView from './components/Owner/OwnerView.jsx';
 
-
 const App = () => {
-    const history = useHistory();
-    const [gId, setId] = useState('');
-    const [profileImage, setProfileImage] = useState('');
-    const [username, setUsername] = useState('');
-    const [gEmail, setGEmail] = useState('');
-    const [mapLatLng, setMapLatLng] = useState('');
-    const [userId, setUserId] = useState('');
-    const [barId, setBarId] = useState();
+  const history = useHistory();
+  const [gId, setId] = useState('');
+  const [profileImage, setProfileImage] = useState('');
+  const [username, setUsername] = useState('');
+  const [gEmail, setGEmail] = useState('');
+  const [mapLatLng, setMapLatLng] = useState('');
+  const [userId, setUserId] = useState('');
+  const [barId, setBarId] = useState();
 
-    // useEffect(() => {
-    //     if (localStorage.customerToken) {
-    //         history.push('/customer')
-    //     } else if (localStorage.ownerToken) {
-    //         history.push('/owner')
-    //     }
-    // }, []);
+  // useEffect(() => {
+  //     if (localStorage.customerToken) {
+  //         history.push('/customer')
+  //     } else if (localStorage.ownerToken) {
+  //         history.push('/owner')
+  //     }
+  // }, []);
 
-    return (
-        <div>
-            <BrowserRouter>
-                <Switch>
-                    <Route
-                        exact={true}
-                        path="/"
-                        component={LandingPage}
-                    >
-                        <LandingPage
-                            setId={setId}
-                            setProfileImage={setProfileImage}
-                            setUsername={setUsername}
-                            gId={gId}
-                            username={username}
-                            profileImage={profileImage}
-                            setGEmail={setGEmail}
-                            gEmail={gEmail}
-                            mapLatLng={mapLatLng}
-                            setBarId={setBarId}
-                        />
-                    </Route>
-                    <Route
-                        path="/owner"
-                        component={OwnerView}
-                    >
-                        <OwnerView
-                            barId={barId}
-                        />
-                    </Route>
-                    <Route
-                        path="/customer"
-                        component={CustomerView}
-                    >
-                        <CustomerView
-                            gId={gId}
-                            username={username}
-                            userId={userId}
-                            setMapLatLng={setMapLatLng}
-                            setId={setId}
-                            setUsername={setUsername}
-                        />
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    )
-}
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={LandingPage}
+          >
+            <LandingPage
+              setId={setId}
+              setProfileImage={setProfileImage}
+              setUsername={setUsername}
+              gId={gId}
+              username={username}
+              profileImage={profileImage}
+              setGEmail={setGEmail}
+              gEmail={gEmail}
+              mapLatLng={mapLatLng}
+              setBarId={setBarId}
+            />
+          </Route>
+          <Route
+            path="/owner"
+            component={OwnerView}
+          >
+            <OwnerView
+              barId={barId}
+            />
+          </Route>
+          <Route
+            path="/customer"
+            component={CustomerView}
+          >
+            <CustomerView
+              gId={gId}
+              username={username}
+              userId={userId}
+              setMapLatLng={setMapLatLng}
+              setId={setId}
+              setUsername={setUsername}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
--- src/components/SignUp/Form/Owner/Form Components/OwnerInfo.jsx
+++ src/components/SignUp/Form/Owner/Form Components/OwnerInfo.jsx
@@ -1,154 +1,156 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Button, Typography, TextField, FormControl, FormControlLabel, FormLabel, IconButton, Input, InputLabel, InputAdornment, } from '@material-ui/core';
+import {
+  Grid, Button, Typography, TextField, FormControl, FormControlLabel, FormLabel, IconButton, Input, InputLabel, InputAdornment,
+} from '@material-ui/core';
 import { Visibility, VisibilityOff } from '@material-ui/icons';
 import axios from 'axios';
 import clsx from 'clsx';
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    margin: {
-        margin: theme.spacing(1),
-    },
-    withoutLabel: {
-        marginTop: theme.spacing(3),
-    },
-    textField: {
-        width: '25ch',
-    },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: '25ch',
+  },
 }));
 const OwnerInfo = ({
-    setCounter,
-    barName,
-    address,
-    city,
-    state,
-    zip,
-    number,
-    lat,
-    lng,
-    image,
-    capacity,
-    setBarId
+  setCounter,
+  barName,
+  address,
+  city,
+  state,
+  zip,
+  number,
+  lat,
+  lng,
+  image,
+  capacity,
+  setBarId,
 }) => {
-    const history = useHistory();
-    const classes = useStyles();
-    const [username, setUsername] = useState('');
-    const [firstName, setFirst] = useState('');
-    const [lastName, setLast] = useState('');
-    const [email, setEmail] = useState('');
-    const [values, setValues] = useState({
-        password: '',
-        showPassword: false,
-    });
+  const history = useHistory();
+  const classes = useStyles();
+  const [username, setUsername] = useState('');
+  const [firstName, setFirst] = useState('');
+  const [lastName, setLast] = useState('');
+  const [email, setEmail] = useState('');
+  const [values, setValues] = useState({
+    password: '',
+    showPassword: false,
+  });
 
-    const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value });
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const registerBar = () => {
+    const params = {
+      username,
+      firstName,
+      lastName,
+      email,
+      password: values.password,
     };
+    return axios.post('/db/owner/register', { params })
+      .then(({ data }) => data.owner.id)
+      .catch((err) => { console.warn(err); });
+  };
 
-    const handleClickShowPassword = () => {
-        setValues({ ...values, showPassword: !values.showPassword });
+  const createBar = (data) => {
+    console.log(data, 'OWNER ID in OWNER INFO ln76');
+    const bparams = {
+      ownerId: data,
+      barName,
+      address,
+      city,
+      state,
+      zip,
+      number,
+      lat,
+      lng,
+      image,
+      capacity,
     };
+    return axios.post('/db/bar/create', { bparams })
+      .then(({ data }) => {
+        console.log(data, 'BAR DATA');
+        setBarId(data.id);
+      })
+      .catch((err) => { console.warn(err); });
+  };
 
-    const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-    };
+  const handleAll = async () => {
+    const barRegistration = await registerBar();
+    await createBar(barRegistration);
+    await history.push('/owner');
+  };
 
-    const registerBar = () => {
-        const params = {
-            username,
-            firstName,
-            lastName,
-            email,
-            password: values.password,
-        }
-        return axios.post('/db/owner/register', { params })
-            .then(({ data }) => {
-                return data.owner.id;
-            })
-            .catch(err => { console.warn(err) })
-    }
+  return (
+    <Grid container direction="column" justify="center" column="center">
+      <Grid item container direction="row" justify="center" column="center">
+        <Typography variant="subtitle1">
+          Owner Information
+        </Typography>
+      </Grid>
+      <Grid item container direction="row" justify="center" column="center">
+        <TextField id="standard-basic" label="User Name" onChange={(e) => { setUsername(e.target.value); }} />
+      </Grid>
+      <Grid item container direction="row" justify="center" column="center">
+        <TextField id="standard-basic" label="First Name" onChange={(e) => { setFirst(e.target.value); }} />
+        <TextField id="standard-basic" label="Last Name" onChange={(e) => { setLast(e.target.value); }} />
+      </Grid>
+      <Grid item container direction="row" justify="center" column="center">
+        <TextField id="standard-basic" label="Email" onChange={(e) => { setEmail(e.target.value); }} />
+      </Grid>
+      <Grid item container direction="row" justify="center" alignItems="center">
+        <FormControl className={clsx(classes.margin, classes.textField)}>
+          <InputLabel htmlFor="standard-adornment-password"> Choose Password</InputLabel>
+          <Input
+            id="standard-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={(
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+                          )}
+          />
+        </FormControl>
+      </Grid>
+      <Button
+        variant="outlined"
+        onClick={() => {
+          handleAll();
+        }}
+      >
+        Submit
+      </Button>
+    </Grid>
+  );
+};
 
-    const createBar = (data) => {
-        console.log(data, "OWNER ID in OWNER INFO ln76")
-        const bparams = {
-            ownerId: data,
-            barName,
-            address,
-            city,
-            state,
-            zip,
-            number,
-            lat,
-            lng,
-            image,
-            capacity
-        };
-        return axios.post('/db/bar/create', { bparams })
-            .then(({ data }) => {
-                console.log(data, 'BAR DATA')
-                setBarId(data.id)
-            })
-            .catch(err => { console.warn(err) })
-    }
-
-    const handleAll = async () => {
-        const barRegistration = await registerBar();
-        await createBar(barRegistration);
-        await history.push('/owner')
-    }
-
-    return (
-        <Grid container direction="column" justify="center" column="center">
-            <Grid item container direction="row" justify="center" column="center">
-                <Typography variant="subtitle1">
-                    Owner Information
-            </Typography>
-            </Grid>
-            <Grid item container direction="row" justify="center" column="center">
-                <TextField id="standard-basic" label="User Name" onChange={(e) => { setUsername(e.target.value) }} />
-            </Grid>
-            <Grid item container direction="row" justify="center" column="center">
-                <TextField id="standard-basic" label="First Name" onChange={(e) => { setFirst(e.target.value) }} />
-                <TextField id="standard-basic" label="Last Name" onChange={(e) => { setLast(e.target.value) }} />
-            </Grid>
-            <Grid item container direction="row" justify="center" column="center">
-                <TextField id="standard-basic" label="Email" onChange={(e) => { setEmail(e.target.value) }} />
-            </Grid>
-            <Grid item container direction="row" justify="center" alignItems="center">
-                <FormControl className={clsx(classes.margin, classes.textField)}>
-                    <InputLabel htmlFor="standard-adornment-password"> Choose Password</InputLabel>
-                    <Input
-                        id="standard-adornment-password"
-                        type={values.showPassword ? 'text' : 'password'}
-                        value={values.password}
-                        onChange={handleChange('password')}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword}
-                                    onMouseDown={handleMouseDownPassword}
-                                >
-                                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                </FormControl>
-            </Grid>
-            <Button variant="outlined"
-                onClick={() => {
-                    handleAll();
-                }}>
-                Submit
-            </Button>
-        </Grid>
-    )
-}
-
-export default OwnerInfo
+export default OwnerInfo;
