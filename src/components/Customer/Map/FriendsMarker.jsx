d795d716cb6f9fb6a667bdbb1eaa04d97f805e1a
tenhold <tenhold@gmail.com>
2020-11-13 12:19:05 -0600

    (add) alerts now show as alerts and alert type

--- src/components/Customer/Map/FriendsMarker.jsx
+++ src/components/Customer/Map/FriendsMarker.jsx
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
 import { Marker, InfoWindow } from '@react-google-maps/api';
+// import { ReportProblem } from '@material-ui/icons';
+import warning from '../../../../images/warning.png';
 
-const FriendsMarker = ({ friendsLocation: { user_name, latitude, longitude } }) => {
+const FriendsMarker = ({ friendsLocation: { user_name, latitude, longitude, report } }) => {
   const [show, setShow] = useState(false);
+  const iconSelect = report ? warning : 'http://maps.google.com/mapfiles/ms/icons/blue.png';
+
+  // const iconSelect = () => {
+  //   if (report) {
+  //     return ReportProblem;
+  //   }
+  //   return 'http://maps.google.com/mapfiles/ms/icons/blue.png';
+  // };
 
   const handleClick = () => {
     setShow(!show);
@@ -14,11 +24,16 @@ const FriendsMarker = ({ friendsLocation: { user_name, latitude, longitude } }) 
         lng: +longitude,
       }}
       onClick={() => handleClick()}
-      icon={{ url: 'http://maps.google.com/mapfiles/ms/icons/blue.png' }}
+      icon={{ url: iconSelect }}
     >
       {show && (
         <InfoWindow>
-          <h3>{user_name}</h3>
+          {report ? (
+            <h3>{report}</h3>
+          )
+            : (
+              <h3>{user_name}</h3>
+            )}
         </InfoWindow>
       )}
     </Marker>
