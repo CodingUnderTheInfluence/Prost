180fdef2d4952dd601b94ec9f9e8b91ddadf8b72
lschwall <opsparklschwall@gmail.com>
2020-11-16 07:44:39 -0600

    (fix) alerts bug

--- src/components/Owner/AlertEntry.jsx
+++ src/components/Owner/AlertEntry.jsx
@@ -2,21 +2,20 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const AlertEntry = ({ customer, barId, count }) => {
+  const [state, setState] = useState();
 
-    const [state, setState] = useState();
+  useEffect(() => {
+    axios.get(`/db/drinks/alerts?customer=${customer}&barId=${barId}&count=${count}`)
+      .then(({ data }) => {
+        if (data > 8) setState(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get(`/db/drinks/alerts?customer=${customer}&barId=${barId}&count=${count}`)
-            .then(({ data }) => {
-                setState(data)
-            })
-    }, [])
+  return (
+    <div>
+      {state}
+    </div>
+  );
+};
 
-    return (
-        <div>
-            {state}
-        </div>
-    )
-}
-
-export default AlertEntry
+export default AlertEntry;
