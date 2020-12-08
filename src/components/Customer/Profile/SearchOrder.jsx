e2f9e5c6c945d8058d4b2a6fd388d3ce44ae2f17
Bri-b <bri.skinner02@gmail.com>
2020-12-07 23:08:50 -0600

    (fix) match styles

--- src/components/Customer/Profile/SearchOrder.jsx
+++ src/components/Customer/Profile/SearchOrder.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Grid,
   Typography,
@@ -16,30 +16,79 @@ import Menu from './Menu.jsx';
 export default function SearchOrder({
   menus, setPref, handleChange, list, classes, order, pref,
 }) {
+  const [selectedBar, setSelectedBar] = useState('');
   return (
-    <span>
-      <Grid>
-        <Typography>
-          Preferred Language
-        </Typography>
-        <Language setPref={setPref} />
-      </Grid>
-      <FormControl className={classes.formControl}>
-        <InputLabel id="demo-simple-select-label">Bars</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value="bar"
-          onChange={handleChange}
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+    >
+      <Grid
+        item
+        container
+        direction="column"
+        justify="center"
+        alignItems="center"
+        className={classes.parent}
+      >
+        <Grid
+          item
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
         >
-          {list.map((barObj, key) => <MenuItem key={key} value={barObj.id}>{barObj.bar_name}</MenuItem>)}
-        </Select>
-      </FormControl>
-      <Grid>
-        Menu
-        {menus && menus.map((obj, key) => <Menu order={order} obj={obj} key={key} pref={pref} />)}
+          <Typography variant="subtitle1">
+            Target Language
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Language setPref={setPref} />
+        </Grid>
+        <Grid
+          item
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          className={classes.container}
+        >
+          <Grid
+            item
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <FormControl className={classes.formControl}>
+              <InputLabel id="demo-simple-select-label">Bars</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={selectedBar}
+                onChange={(e) => {
+                  handleChange(e);
+                  setSelectedBar(e.target.value);
+                }}
+              >
+                {list.map((barObj, key) => <MenuItem key={key} value={barObj.id}>{barObj.bar_name}</MenuItem>)}
+              </Select>
+            </FormControl>
+            <Grid>
+              Menu
+              {menus && menus.map((obj, key) => <Menu order={order} obj={obj} key={key} pref={pref} />)}
+            </Grid>
+          </Grid>
+        </Grid>
       </Grid>
-    </span>
+    </Grid>
   );
 }
 
