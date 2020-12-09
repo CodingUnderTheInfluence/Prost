c9bd7b76b6c53db4fb4fbe80322de1c0287f6bb8
lschwall <opsparklschwall@gmail.com>
2020-12-09 12:52:37 -0600

    (update) owner side style and functionality

--- src/components/Owner/Alerts.jsx
+++ src/components/Owner/Alerts.jsx
@@ -1,21 +1,38 @@
-import React, { useEffect } from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { Grid, Typography, Button } from '@material-ui/core';
 import PhoneIcon from '@material-ui/icons/Phone';
 import axios from 'axios';
 import AlertEntry from './AlertEntry.jsx';
 
-const Alerts = ({ barId, customerList, count }) => {
-  let blank;
+const Alerts = ({ barId }) => {
+  const [customerList, setCustomerList] = useState([]);
+
+  /*
+    This function gets customers that are currently at the bar
+  */
+  // const fetchCustomers = () => {
+  //   axios.get(`/db/cb/list?barId=${barId}`)
+  //     .then(({ data }) => {
+  //       setCustomerList(data);
+  //     })
+  //     .catch((err) => {
+  //       console.warn(err);
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   fetchCustomers();
+  // }, []);
 
   return (
     <Grid container direction="column" justify="center" alignItems="center">
       <Grid item container direction="row" justify="center" alignItems="center">
-        <Typography variant="h5">
+        <Typography variant="subtitle1">
           Stop serving these customers
         </Typography>
       </Grid>
       <Grid item container direction="row" justify="center" alignItems="center">
-        {customerList.map((customer) => <AlertEntry customer={customer} barId={barId} count={count} />)}
+        {customerList.map((customer) => <AlertEntry customer={customer} barId={barId} />)}
       </Grid>
     </Grid>
   );
--- src/components/Owner/BarCustomerList.jsx
+++ src/components/Owner/BarCustomerList.jsx
@@ -1,8 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { Grid, Typography } from '@material-ui/core';
+import axios from 'axios';
 import CustomerList from './CustomerList.jsx';
 
-const BarList = ({ customerList, barId }) => {
+const BarList = ({ barId }) => {
+  const [customerList, setCustomerList] = useState([]);
+
+  const fetchCustomers = () => {
+    axios.get(`/db/cb/list?barId=${barId}`)
+      .then(({ data }) => {
+        setCustomerList(data);
+      })
+      .catch((err) => {
+        console.warn(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchCustomers();
+  }, []);
+
   if (customerList.length === 0) {
     return (
       <Grid container direction="column" justify="center" alignItems="center">
